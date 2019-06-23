39645880c37fd414066b89de053cd04fb936924e
thecontinium <thecontinium@outlook.com>
2019-06-23 11:34:59 +0800

    added more tmux config and tmuxp

---
+++ tmuxp/zsh/env_tmuxp.zsh
@@ -0,0 +1,4 @@
+# Output the file name if it is executed at login
+[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
+
+export TMUXP_CONFIGDIR=${XDG_CONFIG_HOME}/tmuxp
