cb2beaaaafa2fe9ae45e4c11d7dc644036d72ce9
thecontinium <thecontinium@outlook.com>
2019-07-13 15:08:46 +0800

    init submodes

--- update-git-submodules-to-head.sh
+++ update-git-submodules-to-head.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Building all submodules while checking out from MASTER branch."
-
-git submodule update 
-git submodule foreach git checkout master 
-git submodule foreach git pull origin master 
+git submodule init
+git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull origin master
