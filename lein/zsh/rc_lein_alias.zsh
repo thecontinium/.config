ff69cb3a4f4bcc9cf2badf86eaec8ebfb2391b42
thecontinium <thecontinium@outlook.com>
2019-07-13 15:29:24 +0800

    create .lein directory if it does not exist

--- lein/zsh/rc_lein_alias.zsh
+++ lein/zsh/rc_lein_alias.zsh
@@ -4,4 +4,5 @@
 # To make lein use the correct configuration file we need to hard wire
 # the default location.
 # We do this if the .lein/project.clj file does not exist in out home directory
+! [[ -d $HOME/.lein ]] && mkdir $HOME/.lein
 ! [[ -a  $HOME/.lein/profiles.clj ]] && ln -s $XDG_CONFIG_HOME/lein/profiles.clj $HOME/.lein/profiles.clj
