0e74c5a7e6f55effd07c6f8cdf453bda6823f3a5
thecontinium <thecontinium@outlook.com>
2019-07-16 17:43:29 +0800

    make prepl work with nrepl in lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,1 +1,5 @@
-{:repl {:plugins [[cider/cider-nrepl "0.22.0-beta8"]]}}
+{:repl {
+        :repl-options { :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}
+        ;; :plugins [[cider/cider-nrepl "0.22.0-beta8"]]
+        }
+}
