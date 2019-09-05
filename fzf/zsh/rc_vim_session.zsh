45350df898fa66b3b93f08543ad018f0319c8934
thecontinium <thecontinium@outlook.com>
2019-09-05 17:06:42 +0800

    moved sessions to sync

--- fzf/zsh/rc_vim_session.zsh
+++ fzf/zsh/rc_vim_session.zsh
@@ -3,14 +3,14 @@
 vs() {
 	local file='' prompt='(Choose session): ' header='────────────'
 
-	file=$(gfind "$XDG_CACHE_HOME/vim/session" -type f -printf '%Cs %f\n' \
+	file=$(gfind "$HOME/Resilio Sync/app/vim/session" -type f -printf '%Cs %f\n' \
 				| sed 's/\.vim$//g' \
 				| sort -r \
 				| awk "{print \$2}" \
 				| fzf -0 --no-multi --no-sort --prompt="$prompt" --header="$header")
 
 	if [ -n "$file" ] && [ "$?" = "0" ]; then
-		nvim -S "$XDG_CACHE_HOME/vim/session/${file}.vim"
+		nvim -S "$HOME/Resilio Sync/app/vim/session/${file}.vim"
 	else
 		echo 'nothing selected, abort'
 	fi
