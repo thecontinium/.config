58903f6590465273c7b5640ce39ce1510c253f92
thecontinium <thecontinium@outlook.com>
2019-11-10 12:21:49 +0800

    added atom

---
+++ atom/zsh/rc_atom_alias.zsh
@@ -0,0 +1,6 @@
+# Output the file name if it is executed at login
+[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
+
+# To make use the correct configuration file we need to hard wire
+# the default location.
+export ATOM_HOME=$XDG_CACHE_HOME/atom
