4b47a3ffa7a40d9dde83b47d7e844b3121c047d7
thecontinium <thecontinium@outlook.com>
2020-03-15 19:05:46 +0000

    add jenv and lein to increase timeout for all repl

---
+++ jenv/zsh/env_jenv.zsh
@@ -0,0 +1,8 @@
+
+# Output the file name if it is executed at login
+[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
+
+export JENV_ROOT=${XDG_CACHE_HOME}/jenv
+# Added to enable shims and autocompletion for jenv installed through homebrew
+export PATH="$JENV_ROOT/bin:$PATH"
+eval "$(jenv init -)"
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl {
-        :repl-options { :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}
+        :repl-options { :timeout 120000 :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}
         ;; :plugins [[cider/cider-nrepl "0.22.0-beta8"]]
         }
 }
