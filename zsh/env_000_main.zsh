bda7e90622b9b1519c6d26906a64f5daa1bf0fc7
thecontinium <thecontinium@outlook.com>
2020-04-27 15:51:53 +0100

    added alacritty+terminfo

--- zsh/env_000_main.zsh
+++ zsh/env_000_main.zsh
@@ -9,3 +9,4 @@ if [ -x /usr/libexec/path_helper ]; then
 	eval `/usr/libexec/path_helper -s`
 fi
 
+export PREFIX=/usr/local
---
+++ zsh/env_terminfo.zsh
@@ -0,0 +1,5 @@
+# Look for terminfo files under data
+
+export TERMINFO="$XDG_DATA_HOME/terminfo"
+export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:$PREFIX/share/terminfo"
+
