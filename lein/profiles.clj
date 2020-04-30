efe46c0ea8c23b2715fbd4e6855160ee28814d4c
thecontinium <thecontinium@outlook.com>
2020-04-30 18:08:22 +0100

    add tmux config and use latest conjure

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl {
-        :repl-options { :timeout 120000 :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}
-        ;; :plugins [[cider/cider-nrepl "0.22.0-beta8"]]
+        ; :repl-options { :timeout 120000 :init (clojure.core.server/start-server {:accept 'clojure.core.server/io-prepl :address "localhost" :port 55555 :name "lein"})}
+        :plugins [[cider/cider-nrepl "0.22.0"]] ; https://docs.cider.mx/cider/basics/middleware_setup.html
         }
 }
