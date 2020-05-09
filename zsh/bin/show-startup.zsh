77b55ed12be2e78fc4e74eb445375389080ca8d5
thecontinium <thecontinium@outlook.com>
2020-05-09 16:03:38 +0100

    enforce setup

--- zsh/bin/show-startup.zsh
+++ zsh/bin/show-startup.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 local CONFIG_ZSH_DRY_RUN=1
-[[ ! -f /etc/zshenv ]] && sudo cp $HOME/.config/zshenv /etc/zshenv
-echo “—-/etc/—-“; source /etc/zshenv
-for startup_file in zshenv zprofile zshrc zlogin zlogout; do echo “—-$startup_file—-“; source $ZDOTDIR/.$startup_file ; done
-
+if [[ ! -f /etc/zshenv ]]
+then
+  echo "Run setup first"
+else
+  echo “—-/etc/—-“; source /etc/zshenv
+  for startup_file in zshenv zprofile zshrc zlogin zlogout; do echo “—-$startup_file—-“; source $ZDOTDIR/.$startup_file ; done
+fi
