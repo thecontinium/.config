d6884099759cd0f05d22f5d30b5b6a8414a4eab4
thecontinium <thecontinium@outlook.com>
2020-07-07 13:17:54 +0100

    make keychain load all keys

--- zsh/rc_keychain.zsh
+++ zsh/rc_keychain.zsh
@@ -4,5 +4,8 @@
 ## Use keychain to persist keys
 # https://github.com/funtoo/keychain
 if hash keychain 2>/dev/null; then
-  eval "$(keychain --dir "$XDG_CACHE_HOME/keychain" --eval --agents ssh -Q --quiet current)"
+  # eval "$(keychain --dir "$XDG_CACHE_HOME/keychain" --eval --agents ssh -Q --quiet current)"
+  public_keys=$(cd ~/.ssh; ls -m *.pub | tr ',\n' ' ' | sed 's/.pub //g' )
+  cmd="keychain --dir ${XDG_CACHE_HOME}/keychain --eval --agents ssh --quiet ${public_keys}"
+  eval $(eval "${cmd}")
 fi
