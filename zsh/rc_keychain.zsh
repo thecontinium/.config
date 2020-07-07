dcef9be60401f160ab658d11068344ddf7ad55d5
thecontinium <thecontinium@outlook.com>
2020-07-07 14:23:37 +0100

    moved keys to a directory

--- zsh/rc_keychain.zsh
+++ zsh/rc_keychain.zsh
@@ -5,7 +5,7 @@
 # https://github.com/funtoo/keychain
 if hash keychain 2>/dev/null; then
   # eval "$(keychain --dir "$XDG_CACHE_HOME/keychain" --eval --agents ssh -Q --quiet current)"
-  public_keys=$(cd ~/.ssh; ls -m *.pub | tr ',\n' ' ' | sed 's/.pub //g' )
+  public_keys=$(ls -m ~/.ssh/keys/*.pub | tr ',\n' ' ' | sed 's/.pub //g' )
   cmd="keychain --dir ${XDG_CACHE_HOME}/keychain --eval --agents ssh --quiet ${public_keys}"
   eval $(eval "${cmd}")
 fi
