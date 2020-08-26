a34605a5dcb0f8671e71d179763289f3aa892718
thecontinium <thecontinium@outlook.com>
2020-08-26 15:47:44 +0100

    added gattr to reset smudge

--- git/zsh/rc_git_alias.zsh
+++ git/zsh/rc_git_alias.zsh
@@ -2,4 +2,4 @@
 [[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
 
 # https://stackoverflow.com/questions/21652242/git-re-checkout-files-after-creating-smudge-filter
-alias reattr = !sh -c "\"git stash save; rm .git/index; git checkout HEAD -- \\\"$(git rev-parse --show-toplevel)\\\"; git stash pop\""
+alias reattr="git stash save; rm .git/index; git checkout HEAD -- "$(git rev-parse --show-toplevel)"; git stash pop"
