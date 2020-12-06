0ab76c33fa82a26177f78eb810b58c1c4a19786c
thecontinium <thecontinium@outlook.com>
2020-12-06 14:19:33 +0000

    made screeen sharing use the key mappings on the remote

---
+++ apple/rc_defaults.zsh
@@ -0,0 +1,7 @@
+# Output the file name if it is executed at logingAgjj
+[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
+
+if is_mac; then
+  # this belongs is an apple launchctl
+  defaults write com.apple.ScreenSharing DoNotSendSystemKeys -bool No
+fi
