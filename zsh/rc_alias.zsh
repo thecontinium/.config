a963e9666c1fd103ef6fe88cc48bf752ec01c605
thecontinium <thecontinium@outlook.com>
2021-01-01 18:54:52 +0000

    updated alias's

--- zsh/rc_alias.zsh
+++ zsh/rc_alias.zsh
@@ -12,27 +12,101 @@ alias wget='wget --no-check-certificate'
 alias pinst='sudo python setup.py install && sudo rm -r build && sudo rm -r dist && sudo rm -r *egg-info' # install a Python package
 alias beep='echo -n "\a"'
 
-# A quick way to get out of current directory
-alias ..='cd ..'
-alias ...='cd ../../'
-alias ....='cd ../../../'
-alias .....='cd ../../../../'
+# Lists ---------------------------------------------------- l for list -- {{{
 
-# Use GNU ls on macOS instead of BSD
-hash gls 2>/dev/null && LS="gls" || LS="ls"
+LSCMD='ls'
+if is_mac ; then
+	# Use GNU tools on OSX instead of BSD
+	hash gdircolors 2>/dev/null && alias dircolors='gdircolors'
+	hash gfind 2>/dev/null && alias find='gfind'
+	hash gsort 2>/dev/null && alias sort='gsort'
+	hash gstat 2>/dev/null && alias stat='gstat'
+	hash gls 2>/dev/null && LSCMD='gls'
+fi
 
-# Listing directory contents
-alias ls='LC_COLLATE=C '$LS' --color=auto --group-directories-first'
+alias ls="LC_COLLATE=C ${LSCMD} --color=auto --group-directories-first"
+unset LSCMD
 alias l='ls -CFa'
 alias ll='ls -alF'
 alias lsd='ls -Gal | grep ^d'
 alias la='ls -la'
 alias lst="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/ /' -e 's/-/|/'"
-unset LS
+alias llh='ls -alFh'
+alias lld='ls -Gal --color=always | grep ^d --colour=never'
+# }}}
+# Editor --------------------------------------------------- e for edit -- {{{
+alias e='tmux split-window -h "$EDITOR"'
 
-hash gdircolors 2>/dev/null && alias dircolors=gdircolors
-hash gsort 2>/dev/null && alias sort=gsort
+# Neo/vim shortcuts, use Neovim by default
+if hash nvim 2>/dev/null; then
+	alias vim=nvim
+	alias vless="nvim -u $PREFIX/share/nvim/runtime/macros/less.vim"
+	alias suvim='sudo -E nvim'
+else
+	alias vless="vim -u $PREFIX/share/vim/vim74/macros/less.vim"
+	alias suvim='sudo -E vim'
+fi
+
+alias v='vim $(fzf)'
+alias vi=vim
+alias vimdiff='vim -d'
+alias testvim="VIMINIT='let \$MYVIMRC=\"\$HOME/box/testvim/init.vim\" | source \$MYVIMRC' nvim"
+
+# }}}
+# Grepping ------------------------------------------------- p for grep -- {{{
+if hash rg 2>/dev/null; then
+	# Use https://github.com/BurntSushi/ripgrep by default
+	alias p=rg
+elif hash ag 2>/dev/null; then
+	# Fallback to https://github.com/ggreer/the_silver_searcher
+	alias p=ag
+else
+	# Fallback to slow grep
+	alias p=grep
+fi
+
+# Productive defaults for grep and tree
+alias grep='grep --color=auto --exclude-dir=.git'
+alias tree='tree -F --dirsfirst -a -I ".git|.hg|.svn|__pycache__|.mypy_cache|.pytest_cache|*.egg-info|.sass-cache|.DS_Store" --ignore-case'
 
 # Head and tail will show as much possible without scrolling
 hash ghead 2>/dev/null && alias h='ghead -n $((${LINES:-12}-4))'
 hash gtail 2>/dev/null && alias t='gtail -n $((${LINES:-12}-4)) -s.1'
+
+# Use colordiff by default
+hash colordiff 2>/dev/null && alias diff=colordiff
+
+# }}}
+# Explorer --------------------------------------------- = for explorer -- {{{
+
+if hash lf 2>/dev/null; then
+	# Use https://github.com/gokcehan/lf by default
+	= () { lf; }
+elif hash ranger 2>/dev/null; then
+	# Fallback to https://github.com/ranger/ranger
+	= () { ranger; }
+fi
+
+# Easier directory navigation
+alias ..='cd ..'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+alias -- -="cd -"
+
+# }}}
+# Jump around ------------------------------------------- z for jumping -- {{{
+alias cdf='cd "$(dirname "$(fzf)")"'
+
+# }}}
+# File find ------------------------------------------------ f for find -- {{{
+if ! hash fd 2>/dev/null; then
+	# Slow
+	alias f='find . -iname '
+else
+	# Use https://github.com/sharkdp/fd
+	alias f=fd
+fi
+# }}}
+
+#  vim: set ft=sh fdm=marker ts=2 sw=2 tw=80 noet :
