461e40ae05797a70f1accb036da0e68ef3b3cba9
thecontinium <thecontinium@outlook.com>
2021-03-15 11:26:23 +0000

    add nvim + upgrade cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl
- {:plugins [[cider/cider-nrepl "0.25.4"]]}; https://docs.cider.mx/cider/basics/middleware_setup.html
+ {:plugins [[cider/cider-nrepl "0.25.9"]]}; https://docs.cider.mx/cider/basics/middleware_setup.html
  :user
  {:dependencies [[antq/antq "RELEASE"][org.clojure/clojure "1.10.0"]]
   :aliases {"outdated" ["run" "-m" "antq.core"]}}}
