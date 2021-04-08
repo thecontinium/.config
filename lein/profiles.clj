c754fa78136bb60fd6fef80facbc8199310abd1c
thecontinium <thecontinium@outlook.com>
2021-04-08 16:30:40 +0100

    added lein projects

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,4 +2,5 @@
  {:plugins [[cider/cider-nrepl "0.25.9"]]}; https://docs.cider.mx/cider/basics/middleware_setup.html
  :user
  {:dependencies [[antq/antq "RELEASE"][org.clojure/clojure "1.10.0"]]
-  :aliases {"outdated" ["run" "-m" "antq.core"]}}}
+  :aliases {"outdated" ["run" "-m" "antq.core"]}
+  :plugins [[lein-set-version "0.4.1"] [lein-update-dependency "0.1.2"]] }}
