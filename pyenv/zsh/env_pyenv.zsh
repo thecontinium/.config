b61ae914162d2f3a272045e4202da2be9d2de786
thecontinium <thecontinium@outlook.com>
2021-05-27 11:19:19 +0100

    update pyenv

--- pyenv/zsh/env_pyenv.zsh
+++ pyenv/zsh/env_pyenv.zsh
@@ -4,6 +4,3 @@
 export PYENV_ROOT=${XDG_CACHE_HOME}/pyenv 
 path=("$PYENV_ROOT/bin:$PATH" $path)
 export PATH
-# Added to enable shims and autocompletion for pyenv installed through homebrew  
-if which pyenv > /dev/null; then eval "$(pyenv init --path)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
