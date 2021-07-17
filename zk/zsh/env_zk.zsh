5377b7305d290c8d69185df7b4ab6663b57ba270
thecontinium <thecontinium@outlook.com>
2021-07-17 21:54:28 +0100

    added zk

---
+++ zk/zsh/env_zk.zsh
@@ -0,0 +1,4 @@
+# Output the file name if it is executed at login
+[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
+
+export ZK_NOTEBOOK_DIR="$HOME/Library/Mobile Documents/9CR7T2DMDG~com~ngocluu~onewriter/Documents/notesy"
