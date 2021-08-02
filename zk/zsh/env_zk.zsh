79e9181d3a1dd6127ff438e6fb3a557b8d360927
thecontinium <thecontinium@outlook.com>
2021-08-02 12:26:47 +0100

    add vint and 1w

--- zk/zsh/env_zk.zsh
+++ zk/zsh/env_zk.zsh
@@ -1,4 +1,5 @@
 # Output the file name if it is executed at login
 [[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
 
-export ZK_NOTEBOOK_DIR="$HOME/Library/Mobile Documents/9CR7T2DMDG~com~ngocluu~onewriter/Documents/notesy"
+export ONE_WRITER_ICLOUD_DIR="${HOME}/Library/Mobile Documents/9CR7T2DMDG~com~ngocluu~onewriter/Documents"
+export ZK_NOTEBOOK_DIR="${ONE_WRITER_ICLOUD_DIR}/notesy"
