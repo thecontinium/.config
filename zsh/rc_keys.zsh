34e8c6a1713b79dba780c7c8f3a172728aa0bb50
thecontinium <thecontinium@outlook.com>
2022-06-14 14:54:49 +0100

    change bindkeys for history search

--- zsh/rc_keys.zsh
+++ zsh/rc_keys.zsh
@@ -7,10 +7,13 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 
 # Set up/down arrow key search on history
-autoload -U history-search-end
+# autoload -U history-search-end
+autoload -U history-search
 
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
+# zle -N history-beginning-search-backward-end history-search-end
+# zle -N history-beginning-search-forward-end history-search-end
 
-bindkey "\e[A" history-beginning-search-backward-end
-bindkey "\e[B" history-beginning-search-forward-end
+# bindkey "\e[A" history-beginning-search-backward-end
+# bindkey "\e[B" history-beginning-search-forward-end
+bindkey "\e[A" history-beginning-search-backward
+bindkey "\e[B" history-beginning-search-forward
