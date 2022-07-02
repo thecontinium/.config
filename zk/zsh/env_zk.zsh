4cdc4f31a6720144fe3fca8503532e489645c3eb
thecontinium <thecontinium@outlook.com>
2022-07-02 14:17:08 +0100

    use icoud ZK_NOTEBOOK_DIR

--- zk/zsh/env_zk.zsh
+++ zk/zsh/env_zk.zsh
@@ -1,5 +1,5 @@
 # Output the file name if it is executed at login
 [[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
 
-# export ONE_WRITER_ICLOUD_DIR="${HOME}/Library/Mobile Documents/9CR7T2DMDG~com~ngocluu~onewriter/Documents"
-# export ZK_NOTEBOOK_DIR="${ONE_WRITER_ICLOUD_DIR}/notesy"
+export ONE_WRITER_ICLOUD_DIR="${HOME}/Library/Mobile Documents/9CR7T2DMDG~com~ngocluu~onewriter/Documents"
+export ZK_NOTEBOOK_DIR="${ONE_WRITER_ICLOUD_DIR}/notesy"
