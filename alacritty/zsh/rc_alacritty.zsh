fe09cea8487ed6279ef1b2796de814a2b13e7eeb
thecontinium <thecontinium@outlook.com>
2024-03-29 15:49:23 +0000

    use alacritty installed terminfo

--- alacritty/zsh/rc_alacritty.zsh
+++
@@ -1,7 +0,0 @@
-# Output the file name if it is executed at login
-[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
-
-if ! toe | grep -q alacritty; then
-	 curl https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info -so /tmp/alacritty.info
-	 tic -xe alacritty,alacritty-direct /tmp/alacritty.info
-fi
