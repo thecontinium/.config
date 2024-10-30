c0ca8d6c02391a3a58920b32571d1d112e86eeb3
thecontinium <thecontinium@outlook.com>
2024-10-30 14:42:21 +0000

    feat:markdownlint,marksman,kitty

--- markdownlint/zsh/rc_markdownlint_setup.zsh
+++
@@ -1,8 +0,0 @@
-# Output the file name if it is executed at login
-[[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
-
-# To make tmuxp use the correct configuration file for tmux we need to hard wire
-# the default location for tmux.
-# We do this if the .tmux.conf file does not exist in out home directory
-! [[ -a  $HOME/.markdownlintrc ]] && ln -s $XDG_CONFIG_HOME/markdownlint/markdownlint.json $HOME/.markdownlintrc
-
