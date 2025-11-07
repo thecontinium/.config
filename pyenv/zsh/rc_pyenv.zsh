33d66b35225e575e0eed3aafbfdd6a0c0cf78add
thecontinium <thecontinium@outlook.com>
2025-11-07 17:54:12 +1100

    added pyenv setup notebook env

--- pyenv/zsh/rc_pyenv.zsh
+++ pyenv/zsh/rc_pyenv.zsh
@@ -2,3 +2,6 @@
 [[ -v  CONFIG_ZSH_SHOW_EXECUTING ]] && [[ $- == *i* ]] && echo now executing ${(%):-%N}
 
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+
+# add specific commands we want to use 
+zinit ice fbin"setup-notebook-env"; zinit load ${XDG_CONFIG_HOME}/pyenv/bin/
