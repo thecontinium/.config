5c0e335fa95d3b8085654995b1e46607c3232086
thecontinium <thecontinium@outlook.com>
2026-09-17 11:29:44 +1000

    Simplify kitty-tmux term and config to work with neovim

--- zsh/env_terminfo.zsh
+++ zsh/env_terminfo.zsh
@@ -1,5 +1,5 @@
 # Look for terminfo files under data
 
-export TERMINFO="$XDG_DATA_HOME/terminfo"
-export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:$PREFIX/share/terminfo"
+# export TERMINFO="$XDG_DATA_HOME/terminfo"
+# export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:$PREFIX/share/terminfo"
 
